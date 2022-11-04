a62228f931326e63c34ded87289e2cf807bc65c5
John Rey <adioskid@mail.ru>
2022-11-05 01:44:55 +0300

    Homework#5

--- src/InitialArray.java
+++ src/InitialArray.java
@@ -1,9 +1,6 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class InitialArray {
-    String showArray = Arrays.toString(ledger());
-
     public int[] ledger() {
         Random random = new Random();
         int[] mass = new int[5];
