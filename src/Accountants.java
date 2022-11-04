893a82e4a5311be45acc4c51c8a6aa879c94c567
John Rey <adioskid@mail.ru>
2022-11-04 20:10:45 +0300

    Homework#5

--- src/Accountants.java
+++ src/Accountants.java
@@ -1,2 +1,26 @@
+import java.util.Random;
+
 public class Accountants {
+
+    Random random = new Random();
+
+    public int[] ledger() {
+        int[] mass = new int[30];
+        for (int x = 0; x < mass.length; x++) {
+            mass[x] = 100000 + random.nextInt(100000) + 1;
+        }
+        return mass;
+    }
+
+    public static void main(String[] args) {
+        int sum = 0;
+        Accountants acc = new Accountants();
+        int[] total = acc.ledger();
+        for (int t : total) {
+            sum += t;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+
+    }
 }
